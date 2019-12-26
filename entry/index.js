da34d1fbaef3a694fcb1b01fde230fbe73efb61a
guoyujie <296963166@qq.com>
2019-12-26 13:16:28 +0800

    a

--- entry/index.js
+++ entry/index.js
@@ -55,7 +55,7 @@ module.exports = function (args) {
         priter.info('正在进行单元测试>>>>>>>>>>>>>');
 
         let cmd = `${nyc} ${mocha} --require babel-core/register --recursive --reporter spec --bail 
-         ${path.resolve(childProcess.pwd(), './test/**/*.test.js')}`;
+         ${path.resolve(process.pwd(), './test/**/*.test.js')}`;
 
         await execPromise(cmd, {encoding: 'utf8', cwd: process.cwd()})
             .then((data) => {
